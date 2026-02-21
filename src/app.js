8ece32fab67489e2462ac69918cd034500b23c84
amangupta20044 <a.mann20044@gmail.com>
2026-02-21 15:06:22 +0530

    layers-hooks,state,api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const cookieParser= require("cookie-parser")
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())// middleware --esase req.body me data aa jayega
 app.use(cookieParser())// cookie parser as a middleware use hota hai
+
+app.use(cors({
+    credentials:true,
+    origin:"http://localhost:5173"
+}))
 
 // require routes
 const authRouter = require("./routes/auth.route")
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const authController = require("../controllers/auth.controller")
+const identyfyUser = require("../middleware/auth.middleware")
 
 const authRouter = express.Router()
 
+// POST api/auth /register
+
 authRouter.post("/register",authController.registerController)
 
+// POST api/auth /login
+
 authRouter.post("/login",authController.loginController)
+
+//  @route GET api/auth/get-me
+// @desc-- get currently loged in user details
+//  @access private
+
+authRouter.get("/get-me",identyfyUser,authController.getMeController)
 
 module.exports = authRouter
